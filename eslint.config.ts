edb957a6e1bbd55738ed439102ae08ee00694aec
Alexander <adiaz12303@gmail.com>
2025-08-28 23:40:00 -0400

    chore: update configs

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,7 +3,9 @@ import isentinel from "@isentinel/eslint-config";
 export default isentinel({
     react: true,
     rules: {
+		"no-restricted-syntax": "off",
         "no-spaced-func": "off",
+		"ts/no-require-imports": "off",
     },
     spellCheck: false,
     test: true,
